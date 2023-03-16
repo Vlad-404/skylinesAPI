6a7aec1c1812159b62b5ff4be6090a976b4b91cc
Vlad-404 <vmijat21@gmail.com>
2023-03-16 14:19:49 +0100

    Add dotenv package and config/config.env file

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const dotenv = require('dotenv');
 const express = require('express');
+
+// Load environmental variables
+dotenv.config({
+    path: './config/config.env'
+})
 
 const app = express();
 
@@ -7,4 +13,6 @@ app.get('/', (req, res, next) => {
     res.send('<h2>API is online</h2>')
 })
 
-app.listen(3000, () => {console.log('Server running')});
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {console.log(`Server running at port: ${PORT}`)});
