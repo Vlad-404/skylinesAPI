0df797345c9cc95e6fb82f1f03e25d7db4d39800
Vlad-404 <vmijat21@gmail.com>
2023-04-14 19:17:32 +0200

    Add https to production env for token response

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,23 +68,27 @@ exports.loginUser = asyncHandler(async(req, res, next) => {
 
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res, message) => {
-     // Create token
-     const token = user.getSignedJwtToken();
+    // Create token
+    const token = user.getSignedJwtToken();
 
-     const options = {
-        expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
-        ),
-        httpOnly: true
-     };
+    const options = {
+    expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true
+    };
 
-     res
-        .status(statusCode)
-        .cookie('token', token, options)
-        .json({
-            success: true,
-            message: 'User ' + message,
-            token
-        });
+    if(process.env.NODE_ENV === 'production') {
+        options.secure = true
+    }
+
+    res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+        success: true,
+        message: 'User ' + message,
+        token
+    });
 
 };
